54693a34009bdabc43350d40de6151be01affaf7
harlanx <silan.harry@gmail.com>
2024-08-12 21:18:32 +0800

    fix: adaptive appbar contents overflow around background

--- lib/screens/home/adaptive_appbar.dart
+++ lib/screens/home/adaptive_appbar.dart
@@ -1,73 +1,89 @@
 part of '../../screens/home.dart';
 
 class AdaptiveAppbar extends StatelessWidget {
+  const AdaptiveAppbar({
+    super.key,
+    required this.screenWidth,
+  });
+
   final double screenWidth;
-  const AdaptiveAppbar({super.key, required this.screenWidth});
 
   @override
   Widget build(BuildContext context) {
-    return ConstrainedBox(
-      constraints: const BoxConstraints(minHeight: 80),
-      child: DefaultTextStyle(
-        style: const TextStyle(
-          color: Colors.white,
-          fontFamily: 'Poppins',
-        ),
-        child: Container(
-          width: double.infinity,
-          color: Colors.black26,
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Wrap(
-            alignment: screenWidth >= 1240
-                ? WrapAlignment.spaceBetween
-                : WrapAlignment.center,
-            direction: Axis.horizontal,
-            runAlignment: WrapAlignment.center,
-            crossAxisAlignment: WrapCrossAlignment.center,
-            children: [
-              InkWell(
-                onTap: () {},
-                child: Container(
-                  height: 80,
-                  padding: const EdgeInsets.symmetric(vertical: 10),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Image.asset(
-                        'assets/icons/harmony_logo.png',
-                        filterQuality: FilterQuality.medium,
-                        fit: BoxFit.fitHeight,
-                      ),
-                      const SizedBox(width: 10),
-                      const Text(
+    return DefaultTextStyle(
+      style: const TextStyle(
+        color: Colors.white,
+        fontFamily: 'Poppins',
+      ),
+      child: Container(
+        width: double.infinity,
+        color: Colors.black26,
+        constraints: const BoxConstraints(
+            minHeight: 80,
+            maxHeight: double.infinity,
+            maxWidth: double.infinity),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: Wrap(
+          alignment: screenWidth >= 1240
+              ? WrapAlignment.spaceBetween
+              : WrapAlignment.center,
+          direction: Axis.horizontal,
+          runAlignment: WrapAlignment.center,
+          crossAxisAlignment: WrapCrossAlignment.center,
+          children: [
+            InkWell(
+              onTap: () {},
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Flexible(
+                    flex: 1,
+                    child: Image.asset(
+                      'assets/icons/harmony_logo.png',
+                      filterQuality: FilterQuality.medium,
+                      fit: BoxFit.scaleDown,
+                      height: 64,
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  const Flexible(
+                    flex: 2,
+                    child: FittedBox(
+                      fit: BoxFit.scaleDown,
+                      child: Text(
                         'HARMONY',
                         style: TextStyle(
                           fontFamily: 'Monoton',
                           fontSize: 40,
                         ),
+
+                        maxLines: 1,
+                        //overflow: TextOverflow.fade,
                       ),
-                    ],
+                    ),
                   ),
-                ),
+                ],
               ),
-              ConstrainedBox(
-                constraints: const BoxConstraints(maxWidth: 880),
-                child: Container(
-                  height: 80,
-                  width: double.infinity,
-                  padding: const EdgeInsets.symmetric(vertical: 10),
-                  child: Wrap(
-                    runAlignment: WrapAlignment.center,
-                    alignment: WrapAlignment.spaceBetween,
-                    direction: Axis.horizontal,
-                    spacing: 50,
-                    children: [
-                      menuLabelButton('Home', ''),
-                      menuLabelButton('Stories', ''),
-                      menuLabelButton('Community', ''),
-                      menuLabelButton('About', ''),
-                      menuLabelButton('Contact', ''),
-                      ElevatedButton(
+            ),
+            ConstrainedBox(
+              constraints: const BoxConstraints(maxWidth: 880),
+              child: Container(
+                width: double.infinity,
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Wrap(
+                  runAlignment: WrapAlignment.center,
+                  alignment: WrapAlignment.spaceBetween,
+                  direction: Axis.horizontal,
+                  spacing: 50,
+                  children: [
+                    menuLabelButton('Home', ''),
+                    menuLabelButton('Stories', ''),
+                    menuLabelButton('Community', ''),
+                    menuLabelButton('About', ''),
+                    menuLabelButton('Contact', ''),
+                    FittedBox(
+                      fit: BoxFit.scaleDown,
+                      child: ElevatedButton(
                         style: ElevatedButton.styleFrom(
                           shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(7)),
@@ -86,35 +102,41 @@ class AdaptiveAppbar extends StatelessWidget {
                             fontSize: 22,
                             fontWeight: FontWeight.w500,
                           ),
+                          maxLines: 1,
                         ),
                         onPressed: () {},
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
   }
 
   Widget menuLabelButton(String title, String navigation) {
-    return InkWell(
-      splashColor: Colors.transparent,
-      highlightColor: Colors.transparent,
-      overlayColor: WidgetStateProperty.all(Colors.transparent),
-      child: Text(
-        title,
-        style: const TextStyle(
-          fontSize: 22,
-          fontWeight: FontWeight.w500,
+    return FittedBox(
+      fit: BoxFit.scaleDown,
+      child: InkWell(
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+        overlayColor: WidgetStateProperty.all(Colors.transparent),
+        child: Text(
+          title,
+          style: const TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.w500,
+          ),
+          maxLines: 1,
+          overflow: TextOverflow.fade,
         ),
+        onTap: () {
+          //Go to navigation
+        },
       ),
-      onTap: () {
-        //Go to navigation
-      },
     );
   }
 }
